eea82d8b9de04f2cb3eaf2d523eb735663a49940
Magnus Andersson <bredsjomagnus@yahoo.se>
2017-12-09 17:09:51 +0000

    adding files

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+    Gameboard: require('./lib/Memory/gameboard'),
+    Gamebrain: require('./lib/Memory/gamebrain'),
+    Memorycard: require('./lib/Memory/memorycard'),
+}
